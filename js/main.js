101e947a047f24ecdc99dfd7a0ce27055a07e3d7
Caglar Turali <caglarturali@gmail.com>
2018-11-22 17:11:46 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+  /*
+  $('.home').click(function (e) {
+			$('#nav-home').css({'color':'rgb(0,0,0)','font-weight': '400'})
+			$(".panel-cover").fadeOut();
+    	$('body').css('overflow','auto');
+  })
+
+	if (window.location.pathname == 'home/'){
+		$('#nav-home').css({'color':'rgb(0,0,0)','font-weight': '400'})
+	}
+
+	if (window.location.pathname == 'industry/'){
+		$('#nav-industry').css({'color':'rgb(0,0,0)','font-weight': '400'})
+	}
+
+	if (window.location.pathname == 'research/'){
+		$('#nav-research').css({'color':'rgb(0,0,0)','font-weight': '400'})
+	}
+
+	if (window.location.pathname == 'tools_and_materials/'){
+		$('#nav-tools').css({'color':'rgb(0,0,0)','font-weight': '400'})
+	}
+
+	if (window.location.pathname == 'articles_and_tutorials/'){
+		$('#nav-articles').css({'color':'rgb(0,0,0)','font-weight': '400'})
+	}
+
+	if (window.location.pathname == 'about_us/'){
+		$('#nav-about').css({'color':'rgb(0,0,0)','font-weight': '400'})
+	}
+
+	$('.new-header .btn-mobile-menu__icon').click(function () {
+		$('.mobile-sidebar').fadeIn(200);
+		$('.mobile-sidebar-container').fadeIn(200);
+  })
+
+	$('.mobile-sidebar .btn-mobile-menu__icon').click(function () {
+		$('.mobile-sidebar').fadeOut(200);
+		$('.mobile-sidebar-container').fadeOut(200);
+  })
+*/
+});
