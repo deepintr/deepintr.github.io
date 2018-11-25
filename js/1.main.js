60cbcad2ffe870c92c31633d21c10343f95bd433
Caglar Turali <caglarturali@gmail.com>
2018-11-26 01:09:03 +0300

    Add English support at the base level

--- js/1.main.js
+++ js/1.main.js
@@ -1,4 +1,34 @@
 $(document).ready(function() {
+  // Mail button click handler.
+  var emailBtn = document.getElementById('email-btn');
+  emailBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    var email_user = 'deepintr';
+    var email_at = '@';
+    var email_host = 'gmail.com';
+
+    var subject = 'deepintr.org';
+    var body_message = 'Merhaba!\n';
+
+    emailwin = window.open(
+      'mailto:' + email_user + email_at + email_host + '?subject=' + subject + '&body=' + body_message,
+      'emailWindow'
+    );
+    if (emailwin && emailwin.open && !emailwin.closed) emailwin.close();
+  });
+
+  var langQuery = window.location.search.toLowerCase();
+  langQuery = langQuery.substring(langQuery.indexOf('=') + 1);
+
+  if (langQuery && langQuery === 'en') {
+    document.title = 'Deepin Turkey | Linux Deepin Turkey Community';
+		document.querySelector('.main-header').innerText = "The Only Linux Deepin User Community in Turkey";
+		document.querySelector('.home').setAttribute('title', document.title);
+    document.querySelector('.title-about').innerText = 'About Us';
+		document.querySelector('.about').setAttribute('title', 'Who we are? What we do?');
+		document.querySelector('#email-btn').setAttribute('title', 'Contact Us');
+  }
   /*
   $('.home').click(function (e) {
 			$('#nav-home').css({'color':'rgb(0,0,0)','font-weight': '400'})
