f512a1040cfe265179314633480a8c9fc5d34b55
Caglar Turali <caglarturali@gmail.com>
2018-12-02 19:07:49 +0300

    Convert all links to clean link

--- js/latest_threads.js
+++ js/latest_threads.js
@@ -4,27 +4,15 @@ $(document).ready(function() {
     tr: [
       [
         '【RESMİ AÇIKLAMA】 LINUX DEEPIN CASUS YAZILIM DEĞİLDİR.',
-        'https://spectrum.chat/deepintr?thread=3b0ae224-2877-413f-818c-87e16ce1a7c4'
+        'https://deepintr.org/?linux-deepin-casus-yazilim-degildir'
       ],
-      ['Sıkça Sorulan Sorular', 'https://spectrum.chat/deepintr?thread=9b557ba5-57e2-411e-a703-e4c8472a3cb3'],
-      [
-        'Linux Deepin Kurulumu (Resimli Anlatım)',
-        'https://spectrum.chat/deepintr?thread=9cbd6db3-1080-45bb-9122-a05c07bb9621'
-      ]
+      ['Sıkça Sorulan Sorular', 'https://deepintr.org/?sss'],
+      ['Linux Deepin Kurulumu (Resimli Anlatım)', 'https://deepintr.org/?linux-deepin-kurulumu']
     ],
     en: [
-      [
-        'Welcome to Linux Deepin Turkey Community',
-        'https://spectrum.chat/deepintr/international?thread=7620b4b9-26ca-4c2c-ab7b-b981c17018f3'
-      ],
-      [
-        'Frequently Asked Questions',
-        'https://spectrum.chat/deepintr/sss-faq?thread=42493eb1-19ad-452c-a0fb-191068b266b6'
-      ],
-      [
-        '【OFFICIAL STATEMENT】LINUX DEEPIN IS NOT SPYWARE',
-        'https://spectrum.chat/deepintr?thread=1715ba89-fe5f-426a-9496-f26f015c214f'
-      ]
+      ['Welcome to Linux Deepin Turkey Community', 'https://deepintr.org/?welcome-to-linux-deepin-turkey-community'],
+      ['Frequently Asked Questions', 'https://deepintr.org/?faq'],
+      ['【OFFICIAL STATEMENT】LINUX DEEPIN IS NOT SPYWARE', 'https://deepintr.org/?linux-deepin-is-not-spyware']
     ]
   };
 
