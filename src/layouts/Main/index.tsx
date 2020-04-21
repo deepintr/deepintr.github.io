cab26a7e858f16d40f523f44f4671c8a7caa3bb2
Caglar Turali <caglarturali@gmail.com>
2020-04-21 22:30:36 +0300

    Add GitHubBadge

--- src/layouts/Main/index.tsx
+++ src/layouts/Main/index.tsx
@@ -2,17 +2,19 @@ import "normalize.css";
 import React from "react";
 import Particles from "react-particles-js";
 import { createUseStyles } from "react-jss";
+import GitHubBadge from "../../components/GitHubBadge";
 import styles from "./styles";
 
 const useStyles = createUseStyles(styles);
 
 const Main: React.FC = ({ children }) => {
-  const particlesConfig = require("../../data/particlesjs-config.json");
+  const particlesConfig = require("../../configs/particlesjs-config.json");
   const classes = useStyles();
 
   return (
     <>
       <div className={classes.root}>{children}</div>
+      <GitHubBadge />
       <Particles className={classes.particles} params={particlesConfig} />
     </>
   );
