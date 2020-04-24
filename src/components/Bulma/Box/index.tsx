8fbbbe40f6567f4cfb7c66d8c86053ad08c07b77
Caglar Turali <caglarturali@gmail.com>
2020-04-24 14:31:28 +0300

    Add glass effect to box

--- src/components/Bulma/Box/index.tsx
+++ src/components/Bulma/Box/index.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import clsx from "clsx";
+import { createUseStyles } from "react-jss";
+import styles from "./styles";
+
+const useStyles = createUseStyles(styles);
 
 export interface BoxProps {
   content?: boolean;
 }
 
 const Box: React.FC<BoxProps> = ({ content, children }) => {
+  const classes = useStyles();
+
   return (
-    <div className={clsx("box", { ["content"]: !!content })}>{children}</div>
+    <div className={clsx("box", classes.boxCustom, { ["content"]: !!content })}>
+      {children}
+    </div>
   );
 };
 
