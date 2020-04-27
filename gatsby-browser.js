f690f22a710b8ae0ad862764f01dfbbe90b76d21
Caglar Turali <caglarturali@gmail.com>
2020-04-27 23:05:06 +0300

    Change extension to scss

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,7 +2,7 @@ import moment from 'moment';
 import 'moment/locale/tr';
 import 'normalize.css';
 import '@fortawesome/fontawesome-free/css/all.css';
-import './src/theme/bulma.sass';
+import './src/theme/bulma.scss';
 
 // Set global locale.
 moment.locale('tr');
