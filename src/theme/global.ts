b806899becc93a637dfce2e9001e4c78704de5fb
Caglar Turali <caglarturali@gmail.com>
2020-05-10 21:23:13 +0300

    Define some global styles

--- src/theme/global.ts
+++ src/theme/global.ts
@@ -1,7 +1,11 @@
 export default {
-  glass: {
-    backgroundColor: 'rgba(255, 255, 255, .25)',
-    backdropFilter: 'blur(6px)',
-    WebkitBackdropFilter: 'blur(6px)',
-  },
+  glass: (alpha: number = 0.5, blurRadius: number = 6) => ({
+    backgroundColor: `rgba(255, 255, 255, ${alpha})`,
+    backdropFilter: `blur(${blurRadius}px)`,
+    WebkitBackdropFilter: `blur(${blurRadius}px)`,
+  }),
+  box: () => ({
+    boxShadow:
+      '0 0.5em 1em -0.125em rgba(10, 10, 10, 0.1), 0 0px 0 1px rgba(10, 10, 10, 0.02)',
+  }),
 };
