5a586d661e50955547acdfa99fbcb9be6b848b14
Caglar Turali <caglarturali@gmail.com>
2020-05-10 15:24:52 +0300

    Define Placement type

--- src/models/Bulma.ts
+++ src/models/Bulma.ts
@@ -15,5 +15,7 @@ export declare namespace Bulma {
     | 'warning'
     | 'danger';
 
+  export type Placement = 'left' | 'right';
+
   export type ColumnSize = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 }
