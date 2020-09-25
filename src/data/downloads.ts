b6334d96e257fe12db56ebe97b43f7db1ab4d47d
Caglar Turali <caglarturali@gmail.com>
2020-09-25 19:33:51 +0300

    Update download links for v20

--- src/data/downloads.ts
+++ src/data/downloads.ts
@@ -2,23 +2,29 @@ import { Menu } from '../models';
 
 export const downloads: Menu.DownloadLink[] = [
   {
-    fileName: 'Deepin 20 Beta',
+    fileName: 'Deepin 20',
     sourceName: 'Deepin Sunucuları',
-    url: 'http://cdimage.deepin.com/releases/20Beta/',
+    url: 'http://cdimage.deepin.com/releases/20/',
   },
   {
-    fileName: 'Deepin 20 Beta',
+    fileName: 'Deepin 20',
     sourceName: 'VeriTeknik',
-    url: 'http://mirror.veriteknik.net.tr/deepin-cd/20Beta/',
+    url: 'http://mirror.veriteknik.net.tr/deepin-cd/20/',
   },
   {
-    fileName: 'Deepin 20 Beta',
+    fileName: 'Deepin 20',
+    sourceName: 'OSDN',
+    url: 'https://osdn.net/projects/deepin/storage/20/',
+  },
+  {
+    fileName: 'Deepin 20',
+    sourceName: 'Sourceforge',
+    url: 'https://sourceforge.net/projects/deepin/files/20/',
+  },
+  {
+    fileName: 'Deepin 20',
     sourceName: 'Google Drive',
-    url: '',
-  },
-  {
-    fileName: 'Deepin 20 Beta',
-    sourceName: 'osdn',
-    url: 'https://osdn.net/projects/deepin/storage/20.0_beta/',
+    url:
+      'https://drive.google.com/drive/folders/1vM-Ud8m63RI3sUIWiKiO3_7DLKzbIkHh',
   },
 ];
