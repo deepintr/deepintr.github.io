f8b356a5c98d47906316cf3466c827262b93319d
Çağlar Turalı <23704367+caglarturali@users.noreply.github.com>
2021-02-02 18:06:52 +0300

    Update download links

--- src/data/downloads.ts
+++ src/data/downloads.ts
@@ -2,22 +2,22 @@ import { Menu } from '../models';
 
 export const downloads: Menu.DownloadLink[] = [
   {
-    fileName: 'Deepin 20',
+    fileName: 'Deepin 20.1',
     sourceName: 'Deepin Sunucuları',
-    url: 'http://cdimage.deepin.com/releases/20/',
+    url: 'http://cdimage.deepin.com/releases/20.1/',
   },
   {
-    fileName: 'Deepin 20',
+    fileName: 'Deepin 20.1',
     sourceName: 'VeriTeknik',
-    url: 'http://mirror.veriteknik.net.tr/deepin-cd/20/',
+    url: 'http://mirror.veriteknik.net.tr/deepin-cd/20.1/',
   },
   {
-    fileName: 'Deepin 20',
+    fileName: 'Deepin 20.1',
     sourceName: 'OSDN',
-    url: 'https://osdn.net/projects/deepin/storage/20/',
+    url: 'https://osdn.net/projects/deepin/storage/20.1_1010/',
   },
   {
-    fileName: 'Deepin 20',
+    fileName: 'Deepin 20.1',
     sourceName: 'Sourceforge',
     url: 'https://sourceforge.net/projects/deepin/files/20.1/',
   },
